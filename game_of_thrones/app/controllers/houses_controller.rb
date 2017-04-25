5d0136a6e2fc1c5eb1f9c45a5e7bd69e4987f5e8
Whitney <whitney.purdum@gmail.com>
2017-04-25 17:05:46 -0400

    update

--- game_of_thrones/app/controllers/houses_controller.rb
+++ game_of_thrones/app/controllers/houses_controller.rb
@@ -8,4 +8,34 @@ class HousesController < ApplicationController
       @house = House.find(params[:id])
   end
 
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    flash[:notice] = " Success! The house was deleted."
+    redirect_to house_path
+  end
+
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    flash[:notice] = " Success! The house was updated."
+    redirect_to house_path
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create!(house_params)
+    flash[:notice] = "Success! Your house was added."
+    redirect_to house_path(@house)
+    end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :summary, :quote)
+  end
+
 end
